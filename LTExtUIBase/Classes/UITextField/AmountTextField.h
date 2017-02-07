60e46e68f71ee5f4517cfa96dc780800759c9826
yjpal <yl21ly@qq.com>
2017-02-07 11:13:44 +0800

    add 单位

--- LTExtUIBase/Classes/UITextField/AmountTextField.h
+++ LTExtUIBase/Classes/UITextField/AmountTextField.h
@@ -11,8 +11,8 @@
 
 @interface AmountTextField : UITextField
 
-@property(nonatomic,assign) double maxValue;//default 100000.00
-@property(nonatomic,assign,readonly) long long fenValue;
-
+@property(nonatomic,assign) double maxValue;//default 100000.00 单位：元
+@property(nonatomic,assign,readonly) long long fenValue;//单位：分
+//单位：元
 - (void)setContentAmount:(NSNumber *)amount;
 @end
